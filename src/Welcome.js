43b1edce5199ee5e37492585dfb2d131d7d48b72
Parry93 <parrinelloemanuele@gmail.com>
2022-10-10 23:45:30 +0200

    es-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,11 +3,13 @@ import Age from "./Age";
 
 class Welcome extends React.Component {
   render() {
+
     return <>
     <p>
        Welcome,{this.props.name}
    </p>
    {!! this.props.age && <Age age={this.props.age} />}
+   
  </>
   }
 }
