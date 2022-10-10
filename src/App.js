9af9c3fc2f725589b31efb04f376f9c9ed01e669
Parry93 <parrinelloemanuele@gmail.com>
2022-10-10 16:06:44 +0200

    props-02

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 
+Welcome.defaultProps = {
+    name: "Gigi"
+};
 class App extends React.Component {
   render() {
     return (
       <div>
         <Hello />
-        <Welcome name="Emaneuele"/>
+        <Welcome />
       </div>
     );
   }
