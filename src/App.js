e81d34c12dfddef9cfe02a20cb24ff8830026203
Parry93 <parrinelloemanuele@gmail.com>
2022-11-15 17:04:10 +0100

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import ClickCounter from "./ClickCounter";
+import ClickTracker from "./ClickTracker";
+import Counter from "./Counter";
+import Hello from "./Hello";
+import InteractiveWelcome from "./InteractiveWelcome";
 import { Welcome } from "./Welcome";
+import { Login } from "./Login";
+import UncontrolledLogin from "./UncontrolledLogin";
+import TodoList from "./TodoList";
+import Container from "./Container";
+import LanguageComponent, { LanguageContext } from "./LanguageContext";
+import Sum from "./Sum";
+import GithubUser from "./GithubUser";
+import GithubUserList from "./GithubUserList";
+
+import CustomCounter from "./UseCounter";
+import { CustomLogin } from "./UseForm";
+import { CarDetails } from "./CarDetails";
+import {Route, BrowserRouter, Routes} from "react-router-dom"
+import DisplayLanguage from "./DisplayLanguage";
+
+
+function App () {
+    
+  
+  return (
+      
+          <Routes>
+              <Route path="/" element={<Welcome name="Emanuele" />} />
+              
+          </Routes>
+          
+        
+      )
+  }
 
 
 
-export class App extends React.Component {
-
-    render() {
-
-        return(
-
-            <>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Welcome name="Emanuele"/>}/>    
-            </Routes>
-            </BrowserRouter>
-            </>
-        )
-    }
-}
+export default App;
