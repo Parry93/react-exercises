d9e5798ea0ec84458c35c8ecc95dba4a0dd011e7
Parry93 <parrinelloemanuele@gmail.com>
2022-11-15 16:07:06 +0100

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,92 +1,22 @@
 import React from "react";
-import ClickCounter from "./ClickCounter";
-import ClickTracker from "./ClickTracker";
-import Counter from "./Counter";
-import Hello from "./Hello";
-import InteractiveWelcome from "./InteractiveWelcome";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import { Welcome } from "./Welcome";
-import { Login } from "./Login";
-import UncontrolledLogin from "./UncontrolledLogin";
-import TodoList from "./TodoList";
-import Container from "./Container";
-import LanguageComponent from "./LanguageContext";
-import Sum from "./Sum";
-import GithubUser from "./GithubUser";
-import GithubUserList from "./GithubUserList";
 
-import CustomCounter from "./UseCounter";
-import { CustomLogin } from "./UseForm";
-import { CarDetails } from "./CarDetails";
 
-Welcome.defaultProps = {
-  name: "John",
-};
-class App extends React.Component {
-  onLogin = (state) => {
-    console.log(state);
-  };
 
-  render() {
-    return (
-      <div>
-        <Container title="this is a component composition ">
-          <Hello />
+export class App extends React.Component {
 
-          <Welcome age={29} />
+    render() {
 
-          <Counter />
+        return(
 
-          <ClickCounter
-            onCounterChange={(counter) => {
-              console.log(counter);
-            }}
-          />
-
-          <ClickTracker />
-
-          <InteractiveWelcome />
-
-          <Login onLogin={this.onLogin} />
-
-          <UncontrolledLogin />
-
-          <TodoList
-            render={(items, removeItem) => {
-              return items.map((item, index) => (
-                <div key={item + index}>
-                  {item}
-
-                  <button
-                    type="button"
-                    onClick={(event) => {
-                      removeItem(event, index);
-                    }}
-                  >
-                    Remove
-                  </button>
-                </div>
-              ));
-            }}
-          />
-
-          <LanguageComponent />
-
-          <Sum />
-
-          <GithubUser />
-
-          <GithubUserList />
-
-          <CustomCounter />
-
-          <CustomLogin />
-
-          <CarDetails
-          initialData={{ model: "FIAT Panda", year: "2010", color: "Blue" }}
-        />
-        </Container>
-      </div>
-    );
-  }
+            <>
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Welcome name="Emanuele"/>}/>    
+            </Routes>
+            </BrowserRouter>
+            </>
+        )
+    }
 }
-export default App;
